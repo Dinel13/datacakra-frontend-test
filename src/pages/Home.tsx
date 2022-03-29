1d7f95f15870b8393b9fcd0c45ae78e603f47318
dinel <solahuddin.hafid.tomaballo@gmail.com>
2022-03-29 17:22:21 +0800

    feat: home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 const Home: FC = () => {
   return (
-    <div className="py-24 flex items-center">
-      HOme
-      <Link to="/turis" className="px-4 py-2 font-medium btn-pri">
-        Turis
+    <div className="wrapper text-center">
+      <h2 className="text-2xl sm:text-4xl mt-10">
+        Temukan dan Kelola{" "}
+        <span className="font-black tracking-widest">Data Turis</span>
+      </h2>
+      <Link to="/turis" className="mt-10 inline-block px-6 py-2 font-medium btn-pri">
+        Lihat Data Turis
       </Link>
     </div>
   );
